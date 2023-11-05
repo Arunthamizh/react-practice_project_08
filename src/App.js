b28f1108bf7aa147136d484a9d5bf6e44a776af2
Arunthamizh-laptop <workattwilight@gmail.com>
2023-11-05 21:54:36 +0530

    add user list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import AddUser from './components/Users/AddUser';
-
-
+import React, { useState } from "react";
+import AddUser from "./components/Users/AddUser";
+import UserList from "./components/Users/UserList";
 
 function App() {
+  const [userList, setUserList] = useState([]);
   return (
     <div>
-      <AddUser/>
+      <AddUser />
+      <UserList users={userList} />
     </div>
   );
-} 
+}
 
 export default App;
