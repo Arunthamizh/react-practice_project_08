04bf423d8443310a31811c16082f032a26f01cf2
Arunthamizh-laptop <workattwilight@gmail.com>
2023-11-05 21:19:18 +0530

    validation and reset the form

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -17,17 +17,26 @@ function AddUser(props) {
   }
 
     const addUserHandler = (event) =>{
-      alert('hi')
         event.preventDefault(); 
+        if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0){
+          return;
+        }
+
+        if(+enteredAge < 1){
+          return;
+        }
+
         console.log(enteredUsername, enteredAge)
+        setEnteredUsername('');
+        setEnteredAge('');
     }
   return (
     <Card className={classes.input}>
     <form onSubmit={addUserHandler}> 
         <label htmlFor='username'>Username</label>
-        <input id='username' type="text" onChange={usernameChangeHandler} />
+        <input id='username' type="text" value={enteredUsername} onChange={usernameChangeHandler} />
         <label htmlFor='age'>Age</label>
-        <input id='age' type='number' onChange={ageChangeHandler}/>	
+        <input id='age' type='number' value={enteredAge} onChange={ageChangeHandler}/>	
         <button type='submit'>Add User</button>
         {/* <Button type="submit">Add User</Button> */}
     </form>
