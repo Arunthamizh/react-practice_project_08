0adc86da3dd6f1a38dc1bd14b6eac48e3e3f9415
Arunthamizh-laptop <workattwilight@gmail.com>
2023-11-05 21:09:29 +0530

    card, button, add user form useSate

---
+++ src/components/UI/Card.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+// import './Card.module.css'
+// or
+import classes from './Card.module.css'
+
+
+function Card(props) {
+  return (
+    // <div className={classes.card}>{props.children}</div>
+    // ! combine the css from the own module and css props
+    <div className={`${classes.card} ${props.className}`}>{props.children}</div>
+
+  )
+}
+
+// Card.propTypes = {}
+
+export default Card
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,17 +1,37 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import Card from '../UI/Card';
+import classes from './AddUser.module.css'
+import Button from '../UI/Button';
 function AddUser(props) {
+
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
+
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value)
+  }
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value)
+  }
+
     const addUserHandler = (event) =>{
-        event.preventDefault();
+      alert('hi')
+        event.preventDefault(); 
+        console.log(enteredUsername, enteredAge)
     }
   return (
-    <form onSubmit={addUserHandler}>
+    <Card className={classes.input}>
+    <form onSubmit={addUserHandler}> 
         <label htmlFor='username'>Username</label>
-        <input id='username' type="text"/>
+        <input id='username' type="text" onChange={usernameChangeHandler} />
         <label htmlFor='age'>Age</label>
-        <input id='age' type='number'/>	
+        <input id='age' type='number' onChange={ageChangeHandler}/>	
         <button type='submit'>Add User</button>
+        {/* <Button type="submit">Add User</Button> */}
     </form>
+    </Card>
   )
 }
 
